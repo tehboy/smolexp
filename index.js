3603e1ce817cd2664c2b333d11ae55b7fd0119a7
tehboy <nathan.boy@gmail.com>
2025-05-01 01:53:39 +0000

    Load up an initial html and js file.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const path = require('path') // Import path module
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, 'index.html')) // Serve the HTML file
+})
+
+// Serve smoltalk.js when /smoltalk.js is requested
+app.get('/smoltalk.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'smoltalk.js')) // Serve the JS file
 })
 
 app.listen(port, () => {
---
+++ smoltalk.js
@@ -0,0 +1,4 @@
+"use strict";
+
+// Insert a <span>Loaded</span> element into the body
+document.body.insertAdjacentHTML('beforeend', '<span>Loaded</span>');
